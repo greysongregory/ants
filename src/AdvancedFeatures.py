0e24a6948f3277a754eec2bb7335c5a119dabb53
Greyson Gregory <grey@hrs1024.wireless-resnet.upenn.edu>
2011-12-07 03:05:31 -0500

    work

--- src/AdvancedFeatures.py
+++ src/AdvancedFeatures.py
@@ -4,12 +4,6 @@ Created on Dec 6, 2011
 @author: Vin
 '''
 
-'''
-Created on Dec 6, 2011
-
-@author: Vin
-'''
-
 from features import FeatureExtractor
 
 class AdvancedFeatures(FeatureExtractor):
@@ -78,9 +72,128 @@ class AdvancedFeatures(FeatureExtractor):
         
     def extractVinFeatures(self, world, state, loc, action):
         pass
-    def extractGreyFeatures(self, world, state, loc, action):
+    
+    def extractBetterThanVinFeatures(self, world, state, loc, action):
+        spaces_to_distance = state.create_moves_required_to_get_attacked()
+        
+        f = list()
+        
+        f.append(self.is_one_from_attack(world, state, loc, action, spaces_to_distance))
+        f.append(self.is_two_from_attack(world, state, loc, action, spaces_to_distance))
+        f.append(self.is_three_from_attack(world, state, loc, action, spaces_to_distance))
+        f.append(self.is_four_from_attack(world, state, loc, action, spaces_to_distance))
+        f.append(self.is_five_from_attack(world, state, loc, action, spaces_to_distance))
+        f.append(self.is_at_least_twice_enemy_army(world, state, loc, action))
+        f.append(self.is_at_least_oneandahalf_enemy_army(world, state, loc, action))
+        f.append(self.is_at_least_equal_enemy_army(world, state, loc, action))
+        f.append(self.is_closest_ant_to_closest_food(world, state, loc, action))
+        f.append(self.is_closer_to_food_than_enemy(world, state, loc, action))
         pass
     
+    
+    
+    def is_closer_to_food_than_enemy(self, world, state, loc, action):
+        food_loc = self.find_closest(world, loc, state.lookup_nearby_food(loc))
+        if food_loc is None:
+            return False
+        closest_enemy_to_food = self.find_closest(world, food_loc, state.lookup_nearby_enemy(food_loc))
+        if closest_enemy_to_food is None:
+            return True
+        enemy_dist = world.manhattan_distance(food_loc, closest_enemy_to_food)
+        ant_dist = world.manhattan_distance(food_loc, loc)
+        
+        return ant_dist < enemy_dist 
+    
+    def is_closest_ant_to_closest_food(self, world, state, loc, action):
+        food_loc = self.find_closest(world, loc, state.lookup_nearby_food(loc))
+        if food_loc is None:
+            return False
+        closest_ant_to_food = self.find_closest(world, food_loc, state.lookup_nearby_friendly(food_loc))
+        
+        return closest_ant_to_food is loc
+    
+    def is_at_least_equal_enemy_army(self, world, state, loc, action):
+        enemies = state.lookup_nearby_enemy(loc)
+        friendlies = state.lookup_nearby_friendly(loc)
+        
+        return (len(friendlies) >= len(enemies))
+    
+    def is_at_least_oneandahalf_enemy_army(self, world, state, loc, action):
+        enemies = state.lookup_nearby_enemy(loc)
+        friendlies = state.lookup_nearby_friendly(loc)
+        
+        return (len(friendlies) >= 1.5*len(enemies))
+    
+    def is_at_least_twice_enemy_army(self, world, state, loc, action):
+        enemies = state.lookup_nearby_enemy(loc)
+        friendlies = state.lookup_nearby_friendly(loc)
+        
+        return (len(friendlies) >= 2*len(enemies))
+    
+    def is_five_from_attack(self, world, state, loc, action, spaces_to_distance):
+        enemy_loc = self.find_closest(world, loc, state.lookup_nearby_enemy(loc))
+        if enemy_loc is None:
+            return False
+        dist = world.euclidian_distance(loc, enemy_loc)
+        spaces = -1
+        for i in range(1, 7):
+            if dist < spaces_to_distance:
+                spaces = i
+        
+        return spaces is 5
+    
+    def is_four_from_attack(self, world, state, loc, action, spaces_to_distance):
+        enemy_loc = self.find_closest(world, loc, state.lookup_nearby_enemy(loc))
+        if enemy_loc is None:
+            return False
+        dist = world.euclidian_distance(loc, enemy_loc)
+        spaces = -1
+        for i in range(1, 7):
+            if dist < spaces_to_distance:
+                spaces = i
+        
+        return spaces is 4
+    
+    def is_three_from_attack(self, world, state, loc, action, spaces_to_distance):
+        enemy_loc = self.find_closest(world, loc, state.lookup_nearby_enemy(loc))
+        if enemy_loc is None:
+            return False
+        dist = world.euclidian_distance(loc, enemy_loc)
+        spaces = -1
+        for i in range(1, 7):
+            if dist < spaces_to_distance:
+                spaces = i
+        
+        return spaces is 3
+        
+    def is_two_from_attack(self, world, state, loc, action, spaces_to_distance):
+        #tests to see if the ant is currently on the "edge" of potential attack radius of an enemy ON ITS NEXT TURN
+        #this guarantee that this ant will not die on next turn
+        enemy_loc = self.find_closest(world, loc, state.lookup_nearby_enemy(loc))
+        if enemy_loc is None:
+            return False
+        dist = world.euclidian_distance(loc, enemy_loc)
+        spaces = -1
+        for i in range(1, 7):
+            if dist < spaces_to_distance:
+                spaces = i
+        
+        return spaces is 2
+                    
+    def is_one_from_attack(self, world, state, loc, action, spaces_to_distance):
+        #tests to see if the ant is one space from the attack radius of an enemy ant 
+        #not engaging the enemy, but if the enemy moves toward us, let the battles begin
+        enemy_loc = self.find_closest(world, loc, state.lookup_nearby_enemy(loc))
+        if enemy_loc is None:
+            return False
+        dist = world.euclidian_distance(loc, enemy_loc)
+        spaces = -1
+        for i in range(1, 7):
+            if dist < spaces_to_distance:
+                spaces = i
+        
+        return spaces is 1
+            
     def extract(self, world, state, loc, action):
         """Extract the three simple features."""
         
@@ -97,13 +210,13 @@ class AdvancedFeatures(FeatureExtractor):
         if enemy_loc is None:
             f.append(False)
         else:
-            f.append(self.moving_towards(world, loc, next_loc, enemy_loc));
+            f.append(self.moving_towards(world, loc, next_loc, enemy_loc))
         
         # Moving towards food
         if food_loc is None:
             f.append(False)
         else:
-            f.append(self.moving_towards(world, loc, next_loc, food_loc));
+            f.append(self.moving_towards(world, loc, next_loc, food_loc))
         
         # Moving towards friendly
         if friend_loc is None:
